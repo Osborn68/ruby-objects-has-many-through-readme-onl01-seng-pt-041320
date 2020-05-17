62af4265f099cd7fc45d6063f6a9ae8101af18f4
Christopher Osborn <z68ozz@gmail.com>
2020-05-17 23:57:43 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,19 @@
 class Meal
+  
+  attr_accessor :name, :customer, :tip 
+  
+  @@all = []
+
+def initialize(waiter, customer, tip)
+  @waiter = waiter
+  @customer = customer
+  @tip = tip 
+  @@all << self 
+end 
+
+def self.all 
+  @@all 
+end
+
 
 end
